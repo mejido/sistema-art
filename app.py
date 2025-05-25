47933806409bafd5e8147f591c8ae0672a9a24ea
mejido <mejido@me.com>
2025-05-25 19:24:25 -0300

    Correccion blueprint csv funcional

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask
 from rutas.usuarios import usuarios
 from rutas.auth import auth
-from rutas.csv import csv_bp
+from rutas.csv import csv  # Blueprint csv
 from rutas.cliente import cliente
 import os
 
 app = Flask(__name__)
-app.secret_key = 'clave_secreta'  # Reemplazar en producción
+app.secret_key = 'clave_secreta'
 
 # Registrar Blueprints
 app.register_blueprint(auth)
 app.register_blueprint(usuarios)
-app.register_blueprint(csv_bp)
+app.register_blueprint(csv)
 app.register_blueprint(cliente)
 
-# Ejecutar app en producción (para Railway)
+# Ejecutar en producción con host y puerto para Railway
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
